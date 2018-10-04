50249557f8a34146f713f4440d9d57d54309d830
migel1976 <migel1976@gmail.com>
2018-10-04 17:05:49 +0300

    продолжаю прописывать роуты

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,18 +3,18 @@ import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import {AuthComponent} from './auth.component';
 import {CommonModule} from '@angular/common';
+import {AuthRoutingModule} from './auth-routing.module';
 
 
 @NgModule({
   declarations:[
-
 LoginComponent,
-
 RegistrationComponent,
 AuthComponent
 ],
 imports:[
-CommonModule
+CommonModule,
+AuthRoutingModule
 ]
 })
 
