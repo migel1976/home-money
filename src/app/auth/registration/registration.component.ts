0b70dda0c639740e2ba88ffa0e8b6c3af6fbde3c
mike smirnoff <migel1976@gmail.com>
2018-10-09 20:13:15 +0300

    реализована проверка почтового ящика на совпадение

--- src/app/auth/registration/registration.component.ts
+++ src/app/auth/registration/registration.component.ts
@@ -18,7 +18,7 @@ export class RegistrationComponent implements OnInit {
 
   ngOnInit() {
 	this.form=new FormGroup(
-		{'email':new FormControl(null,[Validators.required, Validators.email]),
+		{'email':new FormControl(null,[Validators.required, Validators.email],this.forbiddenEmails.bind(this)),
 		 'password':new FormControl(null,[Validators.required, Validators.minLength(6)]),
 		 'name':new FormControl(null,[Validators.required]),
 		 'agree':new FormControl(false,[Validators.required])});
@@ -39,5 +39,19 @@ export class RegistrationComponent implements OnInit {
 				});
 		});
   }
+
+  forbiddenEmails(controls:FormControl):Promise<any>{
+		return new Promise((resolve,reject)=>{
+			this.usersService.getUserByEmail(controls.value)
+				.subscribe((user:User)=>{
+					if(user){
+						resolve({forbiddenemail:true})
+					}
+					else{
+						resolve(null);
+					}
+				});
+	});
+     }
 
 }
