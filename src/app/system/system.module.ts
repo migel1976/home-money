f6f88ac748573049ef60a0e03dab8d13dac2ddf7
migel1976 <migel1976@gmail.com>
2018-10-10 17:43:06 +0300

    начал реализацию заголовка

--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -7,10 +7,12 @@ import { HistoryPageComponent } from './history-page/history-page.component';
 import { PlanningPageComponent } from './planning-page/planning-page.component';
 import { RecordsPageComponent } from './records-page/records-page.component';
 import { SystemComponent} from './system.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
 	imports:[CommonModule, SharedModule, SystemRoutingModule],
-	declarations: [BillPageComponent, HistoryPageComponent, PlanningPageComponent, RecordsPageComponent, SystemComponent],
+	declarations: [BillPageComponent, HistoryPageComponent, PlanningPageComponent, RecordsPageComponent, SystemComponent, SidebarComponent, HeaderComponent],
 })
 
 export class SystemModule{}
