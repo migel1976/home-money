b3cf3f27359ddc9797fe79cbc08065665feda328
migel1976 <migel1976@gmail.com>
2018-10-12 12:53:34 +0300

    работа над baseApi

--- src/app/shared/services/users.service.ts
+++ src/app/shared/services/users.service.ts
@@ -2,13 +2,25 @@ import {Http, Response} from '@angular/http';
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {User} from '../models/user.model'; 
+import {BaseApi} from '../core/base-api';
+
 
 @Injectable()
-export class UsersService{
-	constructor (private http:Http){}
+export class UsersService extends BaseApi{
+	constructor (public http:Http){
+		super(http)
+	}
 
 	getUserByEmail(email:string):Observable<User>{
-		//console.log('getUserByEmail ' + email );
+		return this.get(`users?email=${email}`);
+	}
+	
+	createNewUser(user:User):Observable<User>{
+		return this.post('users',user);
+	}
+
+/*
+	getUserByEmail(email:string):Observable<User>{
 		return this.http.get(`http://localhost:3000/users?email=${email}`)
 		.map((response:Response)=>response.json())
 		.map((user:User[])=>user[0]?user[0]:undefined);
@@ -19,4 +31,5 @@ export class UsersService{
 		return this.http.post('http://localhost:3000/users',user)
 		.map((response:Response)=>response.json());
 	}
+*/
 }
--- src/app/system/shared/services/bill.service.ts
+++ src/app/system/shared/services/bill.service.ts
@@ -2,35 +2,32 @@ import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
 import {Bill} from '../models/bill.model';
-
-
+import {BaseApi} from '../../../shared/core/base-api';
 
 
 @Injectable()
+export class BillService extends BaseApi{
 
-export class BillService{
-
-	constructor(private http:Http){}
-/*
-	getBill():Observable<Bill>{
-		return this.http.get('http://localhost:3000/bill')
-			.map((response:Response)=>response.json());
+	constructor(public http:Http){
+		super(http);
 	}
 
-	getCurrency(base:string='RUB'):Observable<any>{
+	getBill():Observable<Bill>{
+		return this.get('bill');
+	}
 
-		return this.http.get(`http://api.fixer.io/latest?base=${base}`)
-			.map((response:Response)=>response.json());
-	
-	 }
-*/
+
+
+/*	
 	getBill(): Observable<Bill> {
 		    return this.http.get('http://localhost:3000/bill')
 			.map((response:any) => response.json());
 	  }
 
+*/
 	getCurrency(base: string = 'RUB'): Observable<any> {
 		    return this.http.get(`http://data.fixer.io/lates?base=${base}`)
 			.map((response: any) => response.json());
 	  }
+
 }
